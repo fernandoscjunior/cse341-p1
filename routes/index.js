48d056dd83c63b8cd3c2775cfd71a67f1b1620f1
fernandoscjunior <fernandojrsud@icloud.com>
2026-01-12 16:39:54 -0300

    fixing hello world

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.send("Hello world!");
+});
+
 router.use('/contacts', require('./users'));
 
 module.exports = router;
